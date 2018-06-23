4b2e312781210a089491ed79058a7fa82092c24a
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-23 12:53:19 -0600

    Set up variables, wrap in IIFE for security, and add show form button

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
-  // add your code here
-})
+(() => {
+  let newCommentForm, comment, author;
+
+  $(document).ready(function(){
+    let newCommentBtn = $('#new_comment_button');
+    newCommentForm = $('#new_comment');
+
+    comment = $('#new_comment #comment');
+    author = $('#new_comment #author');
+
+    let addCommentBtn = $('#new_comment input[type="submit"]');
+
+    $(newCommentBtn).click(() => {
+      $(newCommentForm).show();
+    });
+  });
+})();
