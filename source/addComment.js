b5e0a182373aed423632e590800b3e30e563ad0b
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-23 12:57:36 -0600

    Add comment function invoked by form submit

--- source/addComment.js
+++ source/addComment.js
@@ -1,17 +1,33 @@
 (() => {
-  let newCommentForm, comment, author;
+  let newCommentForm, comment, author, comments;
 
   $(document).ready(function(){
+    comments = $('#comments');
+
     let newCommentBtn = $('#new_comment_button');
     newCommentForm = $('#new_comment');
 
     comment = $('#new_comment #comment');
     author = $('#new_comment #author');
 
-    let addCommentBtn = $('#new_comment input[type="submit"]');
-
+    // Show the form
     $(newCommentBtn).click(() => {
       $(newCommentForm).show();
     });
+
+    // Handle form submission
+    $(newCommentForm).submit(addComment);
   });
+
+  const addComment = (event) => {
+    event.preventDefault();
+
+    let _comment = buildComment( $(comment).val(), $(author).val() );
+
+    $(comments).append(_comment);
+  };
+
+  const buildComment = (comment, author) => {
+
+  };
 })();
